60fdedd35fdf42ca8a2f18650249f45a4cd887e3
DevChloee <dawon9031@gmail.com>
2022-12-19 23:15:27 -0500

    CHANGE : change readme.md file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/api/customers', (req, res) => {
         {
             'id':1,
             'image': 'http://placeimg.com/64/64/1',
-            'name' : 'Chloe jun',
+            'name' : 'Chloe Jun',
             'birthday' : '940411',
             'gender' : 'female',
             'job' : 'programmer'
@@ -19,7 +19,7 @@ app.get('/api/customers', (req, res) => {
         {
             'id':2,
             'image': 'http://placeimg.com/64/64/2',
-            'name' : 'Sieun Lee',
+            'name' : 'Diana Jean',
             'birthday' : '960212',
             'gender' : 'female',
             'job' : 'student'
@@ -30,6 +30,55 @@ app.get('/api/customers', (req, res) => {
             'birthday' : '900123',
             'gender' : 'female',
             'job' : 'designer'
+        },{
+            'id':4,
+            'image': 'http://placeimg.com/64/64/4',
+            'name' : 'Suzy Bae',
+            'birthday' : '981021',
+            'gender' : 'female',
+            'job' : 'singer'
+        },{
+            'id':5,
+            'image': 'http://placeimg.com/64/64/5',
+            'name' : 'Jieun Lee',
+            'birthday' : '901225',
+            'gender' : 'female',
+            'job' : 'Actress'
+        },{
+            'id':6,
+            'image': 'http://placeimg.com/64/64/6',
+            'name' : 'Sunhwa Han',
+            'birthday' : '881021',
+            'gender' : 'female',
+            'job' : 'Hair designer'
+        },{
+            'id':7,
+            'image': 'http://placeimg.com/64/64/7',
+            'name' : 'Soyoon Jeon',
+            'birthday' : '970712',
+            'gender' : 'female',
+            'job' : 'President'
+        },{
+            'id':8,
+            'image': 'http://placeimg.com/64/64/8',
+            'name' : 'Jihye Lee',
+            'birthday' : '940123',
+            'gender' : 'female',
+            'job' : 'Teacher'
+        },{
+            'id':9,
+            'image': 'http://placeimg.com/64/64/9',
+            'name' : 'Taehee Kim',
+            'birthday' : '86829',
+            'gender' : 'female',
+            'job' : 'Doctor'
+        },{
+            'id':10,
+            'image': 'http://placeimg.com/64/64/10',
+            'name' : 'Yuna Kim',
+            'birthday' : '800214',
+            'gender' : 'female',
+            'job' : 'Professor'
         }
     ]);
 });
